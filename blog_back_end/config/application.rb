77fb2add6579bdd8b5d9dfce6cb11ebc18c6d961
Abdullah Fadhel <abdullahfadhel@outlook.com>
2020-03-11 05:35:57 +0200

    fix application.rb

--- blog_back_end/config/application.rb
+++ blog_back_end/config/application.rb
@@ -19,7 +19,7 @@ require "action_cable/engine"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module PostsTaskBackEnd
+module blog_back_end
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
